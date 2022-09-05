5115ba3705c01c0e5b34bb0a8b7436896cde9f02
Zunaid-jony <jony35666666@gmail.com>
2022-09-05 20:13:31 +0600

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <h1> hello</h1>
+      <Home></Home>
+      
     </div>
   );
 }
