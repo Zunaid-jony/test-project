780c5e4383784ecc6d97dffdec6a83afefb604a8
Zunaid-jony <jony35666666@gmail.com>
2022-09-07 11:08:06 +0600

    setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+     <BrowserRouter>
+     <div className='header'>
+       <NavLink exact activeClassName="active" to="/">Home</NavLink>
+       <NavLink  activeClassName="active" to="/login">Login <small> access without token</small></NavLink>
+
+     </div>
+     <Routes>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/Login' element={<Login></Login>}></Route>
+     </Routes>
+     </BrowserRouter>
       
     </div>
   );
---
+++ src/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            Dashboard pages
+        </div>
+    );
+};
+
+export default Dashboard;
